a54f5d25c7322dff5fe6785387c1e0e80765fa0c
Mahak <56362610+Mahak-G@users.noreply.github.com>
2023-07-05 19:50:24 +0530

    Update server.js

--- MERN Ecommerce Project/server/server.js
+++ MERN Ecommerce Project/server/server.js
@@ -4,14 +4,14 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import Stripe from 'stripe';
 
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
 import Connection from './database/db.js';
 import DefaultData from './default.js';
 import Router from './routes/route.js';
 
 dotenv.config();
 const app=express();
-
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(bodyParser.json({extended: true}));
