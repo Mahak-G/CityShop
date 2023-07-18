0b634fee0820f419844967c1848cc0ed2784549f
Mahak <56362610+Mahak-G@users.noreply.github.com>
2023-07-18 17:08:43 +0530

    Update route.js

--- MERN Ecommerce Project/server/routes/route.js
+++ MERN Ecommerce Project/server/routes/route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { userSignup,userLogin } from '../controller/user-controller.js';
 import { getProducts,getProductById } from '../controller/product-controller.js';
-
+import { enterProduct } from '../controller/entry-controller.js';
 
 const router = express.Router();
 
 router.post('/signup',userSignup);
 router.post('/login',userLogin);
+router.post('/seller',enterProduct);
 
 router.get('/products',getProducts);
 router.get('/product/:id',getProductById);
