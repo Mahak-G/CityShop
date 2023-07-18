ed1fa2edb88e4c32a4ea089aeee989f4545ddcfd
Mahak <56362610+Mahak-G@users.noreply.github.com>
2023-07-18 16:57:41 +0530

    Update slide.jsx

--- MERN Ecommerce Project/client/src/components/home/slide.jsx
+++ MERN Ecommerce Project/client/src/components/home/slide.jsx
@@ -108,8 +108,8 @@ const Slide = ({ products, title, timer }) => {
                         <Link to={`product/${product.id}`} style={{textDecoration: 'none'}}>
                             <Box textAlign="center" style={{ padding: '25px 15px' }}>
                                 <Image src={product.url} alt="product"/>
-                                <Text style={{ fontWeight: 600, color: '#212121' }}>{product.title.shortTitle}</Text>
-                                <Text style={{ color: 'green' }}>{product.discount}</Text>
+                                <Text style={{ fontWeight: 600, color: '#212121' }}>{product.shortTitle}</Text>
+                                <Text style={{ color: 'green' }}>{product.discount_tag}</Text>
                                 <Text style={{ color: '#212121', opacity: '.6' }}>{product.tagline}</Text>
                             </Box>
                         </Link>
